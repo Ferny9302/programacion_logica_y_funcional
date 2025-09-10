615db5919a2f2f728b1b088bbfcf04434ac8bcca
Ferny9302 <111541650+Ferny9302@users.noreply.github.com>
2025-09-10 13:02:18 -0700

    funciones

---
+++ 2_haskell/funciones.hs
@@ -0,0 +1,29 @@
+sumar :: Integer -> Integer -> Integer
+sumar x y= x+y
+
+factorial :: Integer -> Integer
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+menu :: IO()
+menu = do
+    putStrLn("😊😊 == Menu == 😊😊")
+    putStrLn("¿Que deseas hacer?")
+    putStrLn("1) Factorial 👁️")
+    opcion <- getLine
+    
+    case opcion of 
+        "1" -> do
+            putStrLn("Introduce el numero: ")
+            n1 <- getLine
+            let x = read n1 :: Integer
+            putStrLn("El resultado es: "++ show(factorial x) )
+            menu
+        "2"-> do    
+            putStrLn("Bye Bye")
+        _ -> do
+            putStrLn("Opcion no valida")
+            menu
+-- public static void main
+main :: IO ()
+main = menu
